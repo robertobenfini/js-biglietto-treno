96a0141acbd4e2ef670af91609494a01cb7a1598
robertobenfini <roberto.benfini@gmail.com>
2023-05-17 16:46:07 +0200

    aggiunti ;

--- js/script.js
+++ js/script.js
@@ -8,20 +8,20 @@ let km= parseInt (prompt ("Quanti chilometri dovrai percorrere?").trim());
 const prezzo_km=0.21;
 
 //prezzo biglietto intero
-let prezzo_biglietto_intero=(km * prezzo_km).toFixed(2)
+let prezzo_biglietto_intero=(km * prezzo_km).toFixed(2);
 
 //sconto <18
 if (età < 18) {
-    let prezzo_minorenni = (prezzo_biglietto_intero - (prezzo_biglietto_intero * 20 / 100)).toFixed(2)
+    let prezzo_minorenni = (prezzo_biglietto_intero - (prezzo_biglietto_intero * 20 / 100)).toFixed(2);
     document.getElementById ("prezzo").innerHTML = prezzo_minorenni + "€";
 }
 //sconto >=65
 else if (età >= 65) {
-    let prezzo_65 = (prezzo_biglietto_intero - (prezzo_biglietto_intero * 40 / 100)).toFixed(2)
+    let prezzo_65 = (prezzo_biglietto_intero - (prezzo_biglietto_intero * 40 / 100)).toFixed(2);
     document.getElementById ("prezzo").innerHTML = prezzo_65 + "€";
 }
 //prezzo pieno
-else {(prezzo_biglietto_intero)
+else {(prezzo_biglietto_intero);
     document.getElementById ("prezzo").innerHTML = prezzo_biglietto_intero + "€";
 }
 
